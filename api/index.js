8f81eab5b6f214aeb3b1209be467cda20eabfc0d
mishariiigh <mishariiigh@hotmail.com>
2026-09-25 14:45:59 -0400

    update vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
 const app = require('../server');
 
-module.exports = app;
+module.exports = (req, res) => {
+	const requestUrl = new URL(req.url, 'http://localhost');
+	const originalPath = requestUrl.searchParams.get('__originalPath');
+
+	if (originalPath && originalPath.startsWith('/') && !originalPath.startsWith('//')) {
+		req.url = `${originalPath}${requestUrl.searchParams.has('query') ? `?${requestUrl.searchParams.get('query')}` : ''}`;
+	}
+
+	return app(req, res);
+};
